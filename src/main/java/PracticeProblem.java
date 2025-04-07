1ada3b81a0e8a51213ebb76079c812fa21760ef8
kavin-mohanadas-ics4u1-02-25 <kavin.mohanadas@student.tdsb.on.ca>
2025-04-07 15:13:27 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,44 @@ public class PracticeProblem {
 
 	}
 
+	public static int find(int[] array, int number) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == number) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findLast(String[] array, String string) {
+		for (int i = array.length - 1;  i >= 0; i --) {
+			if (array[i].equals(string)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findSecond(char[] array, char character) {
+		int count = 0;
+		int firstIndex = -1;
+		for (int i = 0; i <array.length; i++) {
+			if (array[i] == character) {
+				count ++;
+				if (count == 1) {
+					firstIndex = i;
+				} 
+				else if (count ==2) {
+					return i;
+				}
+			}
+		}
+		if (count == 1) {
+			return firstIndex;
+		}
+		return -1;
+	}
+
 	
 
 }
